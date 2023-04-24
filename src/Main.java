9b534e1b2b78f9114dc32635824195f5364d58a3
larchick <liliyagreis@gmail.com>
2023-04-24 16:33:47 +0300

    2 коммит

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String login = "!love229999999999999999999999456468541947596542";
+        String password = "lilo___1329138_@!";
+        String confirmPassword = "bob1337___@3!&";
+
+        try {
+            Validator.validate(login, password, confirmPassword);
+        } catch (WrongLoginException wrongLoginException) {
+            wrongLoginException.printStackTrace();
+            System.out.println("Получили wrongLoginException");
+        } catch (WrongPasswordException wrongPasswordException) {
+            wrongPasswordException.printStackTrace();
+            System.out.println("Получили wrongPasswordException");
+        } finally {
+            System.out.println("Работает блок finally");
+        }
     }
 }
---
+++ src/Validator.java
@@ -0,0 +1,15 @@
+import java.util.regex.Pattern;
+
+public class Validator {
+    public static void validate(String login, String password, String confirmPassword){
+        if(!Pattern.matches("[a-zA-Z0-9_]{1,20}",login)) {
+            throw new WrongLoginException("Неправильный формат логина");
+        }
+        if(!Pattern.matches("[a-zA-Z0-9_]{1,19}",password)) {
+            throw new WrongLoginException("Неправильный формат пароля");
+        }
+        if (!password.equals(confirmPassword)){
+            throw new WrongLoginException("Введенный пароль неверный");
+        }
+    }
+}
